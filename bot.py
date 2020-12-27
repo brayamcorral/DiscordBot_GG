ec33710e0240e2462c871fa003ef6646845ed02d
Brayam1 <brayamcorral@gmail.com>
2020-12-26 17:27:00 -0800

    Update bot.py
    Prints hello world

--- bot.py
+++ bot.py
@@ -1,2 +1,37 @@
+# bot.py
 import discord
-print("hello world")
+import os 
+import requests
+import json 
+
+BOT_TOKEN = ''
+client = discord.Client()
+
+def get_quote():
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + " -" + json_data[0]['a']
+    return(quote)
+
+@client.event
+async def on_ready():
+    #await client.change_presence(status=discord.Status.idle, activity=discord.Game('IDLE'))
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello World!')
+
+    if message.content.startswith('$test'):
+        await message.channel.send('Working')
+
+    if message.content.startswith('$speak'):
+        quote = get_quote()
+        await message.channel.send(quote)
+
+client.run(BOT_TOKEN)
+

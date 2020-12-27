34acf52f161f9e3e91696264dff6914801e47db8
Brayam1 <brayamcorral@gmail.com>
2020-12-26 18:01:06 -0800

    Add files via upload

---
+++ gunGameEngine.py
@@ -0,0 +1,22 @@
+# Edgar
+class Ai:
+    def __init__(self):
+# Sam 
+def GameEngine:
+    def __init__(self):
+
+# Brayam
+class GunStance:
+    def __init__(self):
+
+# Brayam
+class Player:
+    def __init__(self):
+     
+# Everyone
+def main():
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
